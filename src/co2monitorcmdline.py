f4615201f52c92a5161718dcd9c6f6d94736ac48
Pooj2 <43789321+Pooj2@users.noreply.github.com>
2020-06-01 13:23:54 +0200

    renamed:    co2monitor -> metricsMonitor

--- src/co2monitorcmdline.py
+++ src/co2monitorcmdline.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     dev = usb.core.find(idVendor=Co2Monitor._VID, idProduct=Co2Monitor._PID)
     assert dev is not None
     print(dev._str())
-    co2monitor = Co2Monitor(dev, (sys.argv[1] if len(sys.argv) > 1 else "True") == "True")
-    co2monitor.run()
+    metricsMonitor = Co2Monitor(dev, (sys.argv[1] if len(sys.argv) > 1 else "True") == "True")
+    metricsMonitor.run()
--- src/dbmonitorcmdline.py
+++ src/dbmonitorcmdline.py
@@ -10,5 +10,5 @@ if __name__ == "__main__":
     dev = usb.core.find(idVendor=Amgaze4._VID, idProduct=Amgaze4._PID)
     assert dev is not None
     print(dev._str())
-    co2monitor = Amgaze4(dev, (sys.argv[1] if len(sys.argv) > 1 else "True") == "True")
-    co2monitor.run()
+    metricsMonitor = Amgaze4(dev, (sys.argv[1] if len(sys.argv) > 1 else "True") == "True")
+    metricsMonitor.run()
